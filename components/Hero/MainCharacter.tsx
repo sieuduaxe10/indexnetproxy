1e6264aa7889fae6b27cc3416442fafc36f6cbf0
eric_khanh <khanhdangbmt@gmail.com>
2025-11-18 00:39:51 +0700

    feat: Adjust main character

--- components/Hero/MainCharacter.tsx
+++ components/Hero/MainCharacter.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export const MainCharacter = () => {
   return (
-    <div className="absolute top-[39%] 4xl:top-[51%] 7xl:top-[57%] left-[56%] 4xl:left-[67%] 7xl:left-[64%] translate-x-[-50%] translate-y-[-50%] aspect-[0.950745] w-[363px] h-[382px] 7xl:w-[400px] 7xl:h-[421px]">
+    <div className="absolute top-[61%] 7xl:top-[57%] left-[56%] 4xl:left-[67%] 7xl:left-[64%] translate-x-[-50%] translate-y-[-50%] aspect-[0.950745] w-[363px] h-[382px] 7xl:w-[400px] 7xl:h-[421px]">
       <div className="absolute">
         <Image
           src="/images/hero/goose.png"
