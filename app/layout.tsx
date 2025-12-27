9d24422de3274edab49e17cf6e404843a3fe379b
eric_khanh <khanhdangbmt@gmail.com>
2025-12-27 10:56:54 +0700

    feat: adding thumbnail

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,6 +23,19 @@ export const metadata: Metadata = {
     shortcut: "/images/favicon.png",
     apple: "/images/favicon.png",
   },
+  openGraph: {
+    title: "Residential P2P Proxies – The Best Proxy Solution for MMO",
+    description:
+      "NetProxy.io offers secure, high-performance residential P2P proxies for MMO gamers and marketers. Enjoy global access, low latency, and stable connections with millions of real IPs. Experience ultimate online freedom and reliability.",
+    images: [
+      {
+        url: "/thumbnail.png",
+        width: 1200,
+        height: 630,
+        alt: "NetProxy.io - Residential P2P Proxies",
+      },
+    ],
+  },
 };
 type Locale = (typeof routing.locales)[number];
 
