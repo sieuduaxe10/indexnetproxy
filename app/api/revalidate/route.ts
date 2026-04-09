989f17f602248930b69e7feb7d6fcacb2c6da49a
eric_khanh <khanhdangbmt@gmail.com>
2026-04-09 09:50:11 +0700

    fix: build error

--- app/api/revalidate/route.ts
+++ app/api/revalidate/route.ts
@@ -1,4 +1,4 @@
-import { revalidateTag } from "next/cache";
+import { expireTag } from "next/cache";
 import { type NextRequest, NextResponse } from "next/server";
 
 export async function POST(request: NextRequest) {
@@ -11,12 +11,12 @@ export async function POST(request: NextRequest) {
   const { _type, slug } = body;
 
   if (_type === "post") {
-    revalidateTag("post");
-    revalidateTag("sitemap");
+    expireTag("post");
+    expireTag("sitemap");
   }
 
   if (_type === "category") {
-    revalidateTag("category");
+    expireTag("category");
   }
 
   return NextResponse.json({ revalidated: true, now: Date.now() });
